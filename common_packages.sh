358a63b279d527b4af87706422abb30e9ac4d45a
Sai Srinivas <42400310+saikop99@users.noreply.github.com>
2021-04-21 19:16:07 +0530

    Add files via upload

---
+++ common_packages.sh
@@ -0,0 +1,14 @@
+sudo apt install gcc
+sudo apt install make
+sudo apt install virtualenv
+sudo apt install vim
+sudo apt install curl
+sudo apt install build-essential libsqlite3-dev zlib1g-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev python-tk python3-tk tk-dev libdb4o-cil-dev libbz2-dev lzma liblzma-dev
+sudo apt install npm
+sudo apt-get install libproxy1-plugin-networkmanager gnome-shell-extension-system-monitor
+sudo apt install chrome-gnome-shell
+sudo apt install apt-transport-https
+sudo apt install imwheel
+sudo apt install jupyter-notebook
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
